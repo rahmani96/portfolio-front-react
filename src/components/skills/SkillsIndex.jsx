1d976a71ed1e90ceeb91655e951c18da664cdd01
rahmani96 <islamrahmani96@gmail.com>
2025-07-16 16:40:31 +0100

    modify ui for skills section

--- src/components/skills/SkillsIndex.jsx
+++ src/components/skills/SkillsIndex.jsx
@@ -23,17 +23,20 @@ const SkillsIndex = () => {
         (Object.keys(skills).length === 0 || loading) 
         ? <Loading /> :
         <div className="row offset-1 col-11 offset-md-1 col-md-11 offset-lg-1 col-lg-11 mb-5 mb-sm-5 mb-md-5 mb-lg-0 mb-xl-0 mb-xxl-0">
-                {skills.map((val, i) => (
-                    <div className="col-3 col-md-2 col-lg-2 skill-box">
-                        <div className="mb-3 mt-3" key={i}>
-                            <img style={{display: "block", margin: "auto"}} 
-                                src={val.image} 
-                                width="100px" 
-                                height="80px" 
-                                />
-                            <h6 className="text-uppercase open-sans-font text-center mt-1 mt-sm-3 text-box">{val.name}</h6>
+                {skills
+                    .sort((a, b) => a.index - b.index)
+                    .map((val, i) => (
+                        <div className="col-3 col-md-2 col-lg-2 skill-box">
+                            <div className="mb-3 mt-3" key={i}>
+                                <img style={{display: "block", margin: "auto"}} 
+                                    alt={val.name}
+                                    src={val.image} 
+                                    width="100px" 
+                                    height="80px" 
+                                    />
+                                <h6 className="text-uppercase open-sans-font text-center mt-1 mt-sm-3 text-box">{val.name}</h6>
+                            </div>
                         </div>
-                    </div>
                 ))}
         </div>
     );
